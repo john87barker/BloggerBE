e4b5c3aa9d0ef171acca1dd8b083813f6a87fe80
john87barker <john87barker@gmail.com>
2021-09-03 15:21:59 -0600

    Set up

---
+++ Models/Profile.cs
@@ -0,0 +1,9 @@
+namespace BloggerBE.Models
+{
+    public class Profile
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Picture { get; set; }
+    }
+}
